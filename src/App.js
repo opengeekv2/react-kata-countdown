efaaec63cdb971781ae6b72e0baf98fbe8aafcb3
Marc Mauri Alloza <marcmaurialloza@gmail.com>
2024-06-18 13:34:16 +0200

    countdown and toggling start off and back on

--- src/App.js
+++ src/App.js
@@ -3,11 +3,25 @@ import {useState} from "react";
 
 function App() {
     const [time, setTime] = useState(0);
+    const [startEnabled, setStartEnabled] = useState(true);
 
     const addTime = () => setTime(time+1)
     const substractTime = () => {
-        if (time > 0) setTime(time-1)
+        if (time > 0) setTime(time-1);
     }
+
+    const start = () => {
+        setStartEnabled(false);
+        const intervalId = setInterval(() => {
+            setTime((time) => {
+                if (time > 0) return time - 1;
+                clearInterval(intervalId);
+                setStartEnabled(true);
+                return time;
+            });
+        }, 1000);
+    }
+
   return (
     <>
         <section>
@@ -15,6 +29,7 @@ function App() {
         </section>
         <section>
             <button onClick={addTime}>+</button>
+            <button disabled={!startEnabled} onClick={start}>start</button>
             <button onClick={substractTime}>-</button>
         </section>
     </>
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import App from './App';
-import {render, screen} from "@testing-library/react";
+import {render, screen, waitFor} from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 
 describe('a stopwatch', () => {
@@ -30,6 +30,41 @@ describe('a stopwatch', () => {
     const minusButton = screen.getByRole("button", {name: "-"});
     await userEvent.click(minusButton);
     expect(screen.getByText("0")).toBeInTheDocument();
+  });
+
+  test('it does substract a unit each second when pressing start', async () => {
+    render(<App />);
+    const plusButton = screen.getByRole("button", {name: "+"});
+    await userEvent.click(plusButton);
+    await userEvent.click(plusButton);
+    const startButton = screen.getByRole("button", {name: "start"});
+    await userEvent.click(startButton);
+    expect(await screen.findByText("2")).toBeInTheDocument();
+    expect(await screen.findByText("1")).toBeInTheDocument();
+    expect(await screen.findByText("0")).toBeInTheDocument();
+  });
+
+  test('it disables start button when the countdown starts', async () => {
+    render(<App/>);
+    const plusButton = screen.getByRole("button", {name: "+"});
+    await userEvent.click(plusButton);
+    await userEvent.click(plusButton);
+    const startButton = screen.getByRole("button", {name: "start"});
+    await userEvent.click(startButton);
+    expect(startButton).toBeDisabled();
+  });
+
+  test('it enables start button when the countdown finishes', async () => {
+    render(<App/>);
+    const plusButton = screen.getByRole("button", {name: "+"});
+    await userEvent.click(plusButton);
+    await userEvent.click(plusButton);
+    const startButton = screen.getByRole("button", {name: "start"});
+    await userEvent.click(startButton);
+    expect(await screen.findByText("2")).toBeInTheDocument();
+    expect(await screen.findByText("1")).toBeInTheDocument();
+    expect(await screen.findByText("0")).toBeInTheDocument();
+    await waitFor(() => expect(startButton).toBeEnabled());
   });
 });
 
